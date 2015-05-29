247dba719b9b17d829da9cf19b1e19ae206d40e2
Matteo Calosci <matteo1789@live.it>
2015-05-29 17:50:12 +0200

    è quisto

--- main.c
+++ main.c
@@ -1,8 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define N 3
+#define MAX_STRLEN 25
+
+struct s_ragazza{
+    char nome[MAX_STRLEN+1];
+    char cognome[MAX_STRLEN+1];
+    int cellulare;
+    int eta;
+    int iGradimento;
+};
+typedef struct s_ragazza ragazza;
+
+void funzione (ragazza selezione[], int n, FILE*, FILE*);
 
 int main(int argc, char** argv) {
-
+    
+    int i,j,temp;
+    FILE *evitare,*ripetere;
+    evitare=fopen("daevitare.csv","w");
+    ripetere=fopen("daripetere.csv","w");
+    ragazza selezione[N];
+    
+    for(i=0;i<N;i++){
+        printf("inserisci il nome : ");
+        scanf("\n%[^\n]",selezione[i].nome);
+        printf("inserisci il cognome : ");
+        scanf("\n%[^\n]",selezione[i].cognome);
+        printf("inserisci il numero di telefono: ");
+        scanf("%d",&(selezione[i].cellulare));
+        printf("inserisci l'eta' : ");
+        scanf("%d",&(selezione[i].eta));
+        printf("inserisci il valore di gradimento da 0 a 4 : (<2 da evitare),(>2 da ripetere) ");   
+        scanf("%d",&(selezione[i].iGradimento));
+        }
+    // ordinamento 
+    //for(i=0;i<N-1;i++){
+        //for(j=0;j<N-1-j;j++){
+            //if((strcmp(selezione[j].nome,selezione[j+1].nome)>0){
+                
+               // temp=selezione[j];
+               // selezione[j]=selezione[j+1];
+               // selezione[j+1]=temp;
+                
+           //}
+        //}
+    
+   //}
+    
+    funzione;
+  
+    fclose(evitare);
+    fclose(ripetere);
+    
     return (EXIT_SUCCESS);
 }
 
+void funzione(ragazza selezione[], int n,FILE *evitare, FILE *ripetere){
+    
+    int i;
+    
+    for(i=0;i<n;i++){
+        if( selezione[i].iGradimento<2){
+            fprintf(evitare,"%s,%s,%d,%d,%d",selezione[i].nome,selezione[i].cognome,selezione[i].cellulare,selezione[i].eta,selezione[i].iGradimento);             
+        }
+        else{
+            fprintf(ripetere,"%s,%s,%d,%d,%d",selezione[i].nome,selezione[i].cognome,selezione[i].cellulare,selezione[i].eta,selezione[i].iGradimento);  
+        }
+    
+    
+    }
+    
+
+}
--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -21,8 +21,8 @@ FC=gfortran
 AS=as
 
 # Macros
-CND_PLATFORM=GNU-Linux-x86
-CND_DLIB_EXT=so
+CND_PLATFORM=Cygwin_4.x-Windows
+CND_DLIB_EXT=dll
 CND_CONF=Debug
 CND_DISTDIR=dist
 CND_BUILDDIR=build
@@ -56,9 +56,9 @@ LDLIBSOPTIONS=
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
-	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/ver20150529
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/ver20150529.exe
 
-${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/ver20150529: ${OBJECTFILES}
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/ver20150529.exe: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/ver20150529 ${OBJECTFILES} ${LDLIBSOPTIONS}
 
@@ -73,7 +73,7 @@ ${OBJECTDIR}/main.o: main.c
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
-	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/ver20150529
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/ver20150529.exe
 
 # Subprojects
 .clean-subprojects:
